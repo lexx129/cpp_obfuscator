c9c4731c6c5777ee36972ad9b6680cb66118d45b
lexx129 <Killingspree!!>
2015-12-10 09:40:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * Created by admin on 10.12.2015.
+ */
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\admin\\Documents\\Visual Studio 2010\\Projects\\target\\target"));
+
+    }
+}
